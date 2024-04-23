9568f2361a6cd6347ca8ad3f07a777e802730b3f
CmarxS <caiomarques.cmxs@gmail.com>
2024-04-23 17:53:19 -0300

    Challenge - Front

---
+++ script.js
@@ -0,0 +1,9 @@
+// Função para mostrar o conteúdo dos botões (sprint 2)
+// function mostrarSolucao(element) {
+//     const solucao = element.querySelector('.solucao');
+//     if (solucao.style.display === 'none') {
+//         solucao.style.display = 'block';
+//     } else {
+//         solucao.style.display = 'none';
+//     }
+// }
